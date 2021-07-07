96907d42682578309df106f23d7bf278a289c274
codeAdun <947199625@qq.com>
2021-07-07 17:35:39 +0800

    测试

--- src/main/java/com/sitech/cmi_mall/dao/CategoryMapper.java
+++ src/main/java/com/sitech/cmi_mall/dao/CategoryMapper.java
@@ -2,14 +2,17 @@ package com.sitech.cmi_mall.dao;
 
 import com.sitech.cmi_mall.pojo.Category;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
-@Mapper
 public interface CategoryMapper {
-    @Select("select * from mall_category where id = #{id}")
-    Category findById(@Param("id") Integer id);
+    int deleteByPrimaryKey(Integer id);
 
-    Category queryById(Integer id);
+    int insert(Category record);
 
+    int insertSelective(Category record);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
 }
