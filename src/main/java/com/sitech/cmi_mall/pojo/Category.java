a680f5579dad17f3175fe05d6460f3f65f0a44ff
codeAdun <947199625@qq.com>
2021-07-07 18:03:26 +0800

    测试2

---
+++ src/main/java/com/sitech/cmi_mall/pojo/Category.java
@@ -0,0 +1,22 @@
+package com.sitech.cmi_mall.pojo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Category {
+    private Integer id;
+
+    private Integer parentId;
+
+    private String name;
+
+    private Boolean status;
+
+    private Integer sortOrder;
+
+    private Date createTime;
+
+    private Date updateTime;
+}
