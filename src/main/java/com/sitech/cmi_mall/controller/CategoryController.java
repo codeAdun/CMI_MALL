cde6704a2bb73bb05c23f6dad65c05cf6fbde880
codeAdun <947199625@qq.com>
2021-08-30 09:56:11 +0800

    测试3

---
+++ src/main/java/com/sitech/cmi_mall/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package com.sitech.cmi_mall.controller;
+
+import com.sitech.cmi_mall.service.ICategoryService;
+import com.sitech.cmi_mall.vo.CategoryVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by 廖师兄
+ */
+@RestController
+public class CategoryController {
+
+    @Autowired
+    private ICategoryService categoryService;
+
+    @GetMapping("/categories")
+    public List<CategoryVo> selectAll() {
+        return categoryService.selectAll();
+    }
+}
