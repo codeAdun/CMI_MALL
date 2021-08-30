7c8465fae27e306ff366d2b759ef0d1f245ad525
codeAdun <947199625@qq.com>
2021-08-30 10:15:38 +0800

    Revert "添加静态类"
    This reverts commit 1ba7e0e2

--- src/main/java/com/sitech/cmi_mall/consts/MallConst.java
+++ src/main/java/com/sitech/cmi_mall/consts/MallConst.java
@@ -4,10 +4,7 @@ package com.sitech.cmi_mall.consts;
 //常量类
 public class MallConst {
 
-    //菜单根节点的编号为0
+    //静态类为0
     public static final Integer ROOT_PARENT_ID = 0;
-
-    //
-    public static final String EXCEPTION_NOMENU ="数据加载异常";
 
 }
